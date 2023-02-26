cbf4b9d1ac9b9903280424e931b2134b3a3de62b
rashmi-carol-dsouza <hello@rashmidsouza.com>
2023-02-26 11:49:53 +0100

    server on azure

--- app-server/app.py
+++ app-server/app.py
@@ -1,11 +1,38 @@
-from flask import Flask
-
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory, jsonify
+import json
 app = Flask(__name__)
 
-@app.route("/")
+
+@app.route('/')
 def index():
-    return "Hello World1"
+   print('Request for index page received')
+   return render_template('index.html')
 
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
-if __name__ == "__main__":
-    app.run(debug=True)
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
+       return render_template('hello.html', name = name)
+   else:
+       print('Request for hello page received with no name or blank name -- redirecting')
+       return redirect(url_for('index'))
+
+@app.route('/collaborators', methods=['GET'])
+def get_file():
+    # Read the file in JSON format
+    with open('db.json') as f:
+        data = json.load(f)
+
+    # Return the file as a JSON response
+    return jsonify(data)
+
+if __name__ == '__main__':
+   app.run()
